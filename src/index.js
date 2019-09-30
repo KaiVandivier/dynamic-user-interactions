89825d49d05b1d66dfa3d1ab3e0b13e1f0990042
Kai Vandivier <kpvandivier@gmail.com>
2019-09-30 23:49:06 +0200

    Enable image loading and add skeleton for image carousel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./styles.css";
+import Cardinal from "./northern-cardinal.jpeg";
+import RainbowFlower from "./rainbow-flower.jpg";
+import RedDahlia from "./red-dahlia.jpeg";
+import Flycatcher from "./scissor-tailed-flycatcher.jpg";
 
+/* Drop-down menu */
 const showMenuItems = function showMenuItems(menu) {
   const menuItems = menu.querySelector(".menu-items");
   menuItems.classList.toggle("hidden");
@@ -8,12 +13,12 @@ const showMenuItems = function showMenuItems(menu) {
 const setUpDropDownMenu = function setUpDropDownMenu(menu) {
   const menuButton = menu.querySelector("button.menu-button");
   menuButton.addEventListener("click", () => showMenuItems(menu));
-  console.log("got here");
 };
 
 const navMenu = document.getElementById("nav-menu");
 setUpDropDownMenu(navMenu);
 
+/* Mobile menu */
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileMenuButton = document.getElementById("mobile-menu-button");
 const mobileMenuCloseButton = mobileMenu.querySelector(".close-button");
@@ -28,13 +33,62 @@ mobileMenuButton.addEventListener("click", function() {
 });
 
 mobileMenu.addEventListener("animationend", function(e) {
-  console.log(e);
   if (this.classList.contains("closing")) this.classList.add("hidden");
   if (e.animationName == "menuFrames")
     this.classList.remove("opening", "closing");
-  // this.classList.toggle("hidden");
 });
 
 mobileMenuCloseButton.addEventListener("click", function() {
   mobileMenu.classList.add("closing");
 });
+
+/* Rotating image gallery */
+
+const carouselContainer = document.getElementById("image-carousel");
+const imageContainer = carouselContainer.querySelector(".image-container");
+// const image = imageContainer.querySelector("image");
+// const indexCircleContainer = carouselContainer.querySelector(".index-circle-container");
+// const previousButton = carouselContainer.querySelector("button.previous-button");
+// const nextButton = carouselContainer.querySelector("button.next-button");
+
+const images = [Cardinal, RainbowFlower, RedDahlia, Flycatcher].map(src => {
+  const newImage = new Image();
+  newImage.src = src;
+  return newImage;
+});
+let currentImage = images[0] || null;
+
+//test:
+imageContainer.appendChild(currentImage);
+console.log(imageContainer);
+
+const updateGalleryIndex = function updateGalleryIndex(currentImageIndex) {
+  // query select "index circles" (ICs)
+  // iterate through "ICs"
+  // if you find data-index === currentImageIndex, add "highlight" class
+  // otherwise, remove "highlight" class
+};
+
+const nextImage = function nextImage(currentImageIndex, images) {
+  // do things
+  // const nextImageIndex = currentImageIndex + 1;
+  // const nextImage = images[nextImageIndex]
+  // display nextImage
+  // unhighlight previous "index circle"; highlight next
+  // or: "update gallery index"
+  return { currentImageIndex: currentImageIndex + 1, images };
+};
+
+const previousImage = function previousImage() {
+  // do things
+};
+
+/* add event listener on "previous" and "next" buttons
+previousButton.addEventListener("click", function() {
+  previousImage(currentImageIndex, images)
+});
+nextButton.addEventListener("click", function() {
+  nextImage(currentImageIndex, images)
+});
+
+*/
