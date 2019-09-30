ed3bdca06618f60527cc99c9d7eb1d042814c6d4
Kai Vandivier <kpvandivier@gmail.com>
2019-09-30 20:58:22 +0200

    Add (unmodular) drop-down menu and mobile site menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import "./styles.css";
 
-function myFunction() {
-  // Todo
-}
+const showMenuItems = function showMenuItems(menu) {
+  const menuItems = menu.querySelector(".menu-items");
+  menuItems.classList.toggle("hidden");
+};
+
+const setUpDropDownMenu = function setUpDropDownMenu(menu) {
+  const menuButton = menu.querySelector("button.menu-button");
+  menuButton.addEventListener("click", () => showMenuItems(menu));
+  console.log("got here");
+};
+
+const navMenu = document.getElementById("nav-menu");
+setUpDropDownMenu(navMenu);
+
+const mobileMenu = document.getElementById("mobile-menu");
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const mobileMenuCloseButton = mobileMenu.querySelector(".close-button");
+
+mobileMenuButton.addEventListener("click", function() {
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.remove("hidden");
+    mobileMenu.classList.add("opening");
+  } else {
+    mobileMenu.classList.add("closing");
+  }
+});
+
+mobileMenu.addEventListener("animationend", function(e) {
+  console.log(e);
+  if (this.classList.contains("closing")) this.classList.add("hidden");
+  if (e.animationName == "menuFrames")
+    this.classList.remove("opening", "closing");
+  // this.classList.toggle("hidden");
+});
+
+mobileMenuCloseButton.addEventListener("click", function() {
+  mobileMenu.classList.add("closing");
+});
